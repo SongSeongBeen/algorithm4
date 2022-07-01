c4241eab7002a4f7d9a4ac4eac34be5cbabd411c
송성빈 <aktm1004@naver.com>
2022-07-01 16:36:52 +0900

    Stack

--- src/com/ex/stack/MyStack.java
+++ src/com/ex/stack/MyStack.java
@@ -21,13 +21,5 @@ public class MyStack<T> {
 		return stack.isEmpty();
 	}
 	
-	public static void main(String[] args) {
-		MyStack<Integer> ms = new MyStack<Integer>();
-		ms.push(1);
-		ms.push(2);
-		System.out.println(ms.pop());
-		ms.push(3);
-		System.out.println(ms.pop());
-		System.out.println(ms.pop());
-	}
+
 }
